0e413e8477e6ed0b7120764b6a06fb4b45d27540
Heinrich Chen <47335551+MangoCheesecake@users.noreply.github.com>
2025-01-03 09:37:13 +0800

    Github Pages Deployment

--- backend/routes/word.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getWords } from "../controllers/word.controller.js";
-
-const router = express.Router();
-
-router.get("/:videoKey", getWords)
-
-export default router;
